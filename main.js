593b05f4bdd0c2050670a98660717d0a6bdd88b5
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2022-06-25 16:17:44 +0100

    Refactored JS using sets

--- main.js
+++ main.js
@@ -9,31 +9,14 @@ const allCharsData = `
 Replace this text with the text you want to get the distinct characters from.
 `;
 
-const removeDuplicateChars = (string) => {
-    let charsObject = {};
-    let charsString = "";
-    for (let i = 0; i < string.length; i++) {
-        const currentChar = string[i];
-        if (!charsObject[currentChar]) {
-            charsString += currentChar;
-            charsObject[currentChar] = true;
-        }
-    }
-    return charsString;
-};
-
-const sortString = (string) => {
-    let charsArray = [];
-    for (let i = 0; i < string.length; i++) {
-        charsArray.push(string[i]);
-    }
-    charsArray.sort();
-    return charsArray.join("");
-};
-
-const updateDomElement = () => {
-    phoneticElement.textContent = sortString(removeDuplicateChars(phoneticData));
-    allElement.textContent = sortString(removeDuplicateChars(allCharsData));
+const makeStringOfUniqueChars = (string) => {
+    const set = new Set(string.split(''));
+    return [...set].sort().join('');
 }
 
-updateDomElement();
+const updateDomElements = () => {
+    phoneticElement.textContent = makeStringOfUniqueChars(phoneticData);
+    allElement.textContent = makeStringOfUniqueChars(allCharsData);
+}
+
+updateDomElements();
